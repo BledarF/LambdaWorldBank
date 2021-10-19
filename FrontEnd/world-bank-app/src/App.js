1c9efbc3040fe9988d75ad8a2ed24ffbdfd3b7e9
BledarF <88328574+BledarF@users.noreply.github.com>
2021-10-19 15:59:16 +0100

    Added SearchData Component

--- FrontEnd/world-bank-app/src/App.js
+++ FrontEnd/world-bank-app/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import UserRegistration from "./Components/UserRegistration";
 import UserLogin from "./Components/UserLogin";
+import SearchData from "./Components/SearchData";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>WORLD BANK PROJECT</h1>
+
         <UserLogin />
         <UserRegistration />
+        <SearchData />
       </header>
     </div>
   );
