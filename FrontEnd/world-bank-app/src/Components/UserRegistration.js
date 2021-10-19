48c7c7454687d14e79860ff67ca164b29fab82c4
BledarF <88328574+BledarF@users.noreply.github.com>
2021-10-19 12:15:39 +0100

    Fixed up front end

--- FrontEnd/world-bank-app/src/Components/UserRegistration.js
+++ FrontEnd/world-bank-app/src/Components/UserRegistration.js
@@ -8,22 +8,68 @@ function UserRegistration() {
   const [error, setError] = useState("");
   const url = "http://localhost:7000/api/users";
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const validationResult = validate(username, password, passwordConfirmation);
+    const validationResult = await validate(
+      username,
+      password,
+      passwordConfirmation
+    );
     if (validationResult) {
       setError(validationResult);
     } else {
-      const createAccountResponse = createAccount(
+      const createAccountResponse = await createAccount(
         url,
         username,
         password,
         passwordConfirmation
       );
       console.log(createAccountResponse);
-      setError(createAccountResponse);
+
+      // setError(createAccountResponse);
     }
   };
+
+  async function validate(username, password, passwordConfirmation) {
+    if (!username) {
+      return "Email address is required";
+    } else if (!/\S+@\S+\.\S+/.test(username)) {
+      return "Email address is invalid";
+    } else if (!password) {
+      return "Password is required";
+    } else if (password.length < 9) {
+      return "Password does not exceed 8 characters. Please try again.";
+    } else if (password !== passwordConfirmation) {
+      return "Password does not match";
+    } else {
+      return false;
+    }
+  }
+
+  async function createAccount(url, username, password) {
+    const body = {
+      username: username,
+      password: password,
+    };
+    try {
+      const createAccountResponse = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      const json = await createAccountResponse.json();
+      console.log(json);
+
+      if (!json.msg) {
+        return "That username is taken. Try another.";
+      } else {
+        return "";
+      }
+    } catch (error) {
+      return error;
+    }
+  }
 
   return (
     <form className="log-in-form" onSubmit={handleSubmit}>
@@ -64,48 +110,6 @@ function UserRegistration() {
       {<p id="error-msg">{error}</p>}
     </form>
   );
-}
-
-function validate(username, password, passwordConfirmation) {
-  if (!username) {
-    return "Email address is required";
-  } else if (!/\S+@\S+\.\S+/.test(username)) {
-    return "Email address is invalid";
-  } else if (!password) {
-    return "Password is required";
-  } else if (password.length < 9) {
-    return "Password does not exceed 8 characters. Please try again.";
-  } else if (password !== passwordConfirmation) {
-    return "Password does not match";
-  } else {
-    return false;
-  }
-}
-
-async function createAccount(url, username, password, passwordConfirmation) {
-  const body = {
-    username,
-    password,
-    passwordConfirmation,
-  };
-  try {
-    const createAccountResponse = await fetch(url, {
-      method: "POST",
-      credentials: "include",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    });
-
-    const json = await createAccountResponse.json();
-
-    if (!json.msg) {
-      return "That username is taken. Try another.";
-    } else {
-      return "";
-    }
-  } catch (error) {
-    return error;
-  }
 }
 
 export default UserRegistration;
