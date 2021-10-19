0395c87b1aefbda1d862599015574545a0dee555
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2021-10-19 10:51:16 +0100

    half finished /api/users

--- BackEnd/api/users.js
+++ BackEnd/api/users.js
@@ -4,6 +4,124 @@ const express = require("express");
 const usersRouter = express.Router();
 
 const sqlite3 = require("sqlite3");
-const db = new sqlite3.Database("./lambdadb.sqlite");
+const lambdaDb = new sqlite3.Database("./database/lambdaDb.db");
+
+// Defining userId param
+usersRouter.param("userId", (req, res, next, userId) => {
+  const sql = "SELECT * FROM users WHERE id = $userId";
+  const values = { $userId: userId };
+  lambdaDb.get(sql, values, (error, user) => {
+    if (error) {
+      next(error);
+    } else if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
+// Get all users
+usersRouter.get("/", (req, res, next) => {
+  lambdaDb.all("SELECT * FROM users;", (err, users) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json({ users: users });
+    }
+  });
+});
+
+// Get particular user
+usersRouter.get("/:userId", (req, res, next) => {
+  res.status(200).json({ user: req.user });
+});
+
+// Post a user
+usersRouter.post("/", (req, res, next) => {
+  const username = req.body.user.username;
+  const password = req.body.user.password;
+  const salt = "changeme";
+  const currentDateAndTime = new Date().toString();
+
+  if (!username || !password) {
+    return res.sendStatus(400);
+  }
+
+  const sql = `INSERT INTO users (username, password, salt, created_at, updated_at)
+    VALUES ($username, $password, $salt, $created_at, $updated_at)`;
+  const values = {
+    $username: username,
+    $password: password,
+    $salt: salt,
+    $created_at: currentDateAndTime,
+    $updated_at: currentDateAndTime,
+  };
+
+  lambdaDb.run(sql, values, function (error) {
+    if (error) {
+      next(error);
+    } else {
+      lambdaDb.get(
+        `SELECT * FROM users WHERE users.id = ${this.lastID}`,
+        (error, user) => {
+          res.status(201).json({ user: user });
+        }
+      );
+    }
+  });
+});
+
+// Update a user
+usersRouter.put("/:userId", (req, res, next) => {
+  const username = req.body.user.username;
+  const password = req.body.user.password;
+  const salt = "changeme2";
+  const currentDateAndTime = new Date().toString();
+  if (!username || !password) {
+    return res.sendStatus(400);
+  }
+  const sql =
+    "UPDATE users SET username = $username, password = $password, salt = $salt, updated_at = $updated_at WHERE users.id = $userId";
+  const values = {
+    $username: username,
+    $password: password,
+    $salt: salt,
+    $updated_at: currentDateAndTime,
+  };
+
+  lambdaDb.run(sql, values, (error) => {
+    if (error) {
+      next(error);
+    } else {
+      lambdaDb.get(
+        `SELECT * FROM users WHERE users.id = ${req.params.userId}`,
+        (error, user) => {
+          res.status(200).json({ user: user });
+        }
+      );
+    }
+  });
+});
+
+// Delete a user
+usersRouter.delete("/:userId", (req, res, next) => {
+  const sql = "DELETE FROM user WHERE users.id = $userId";
+  const values = { $userId: req.params.userId };
+
+  lambdaDb.run(sql, values, (error) => {
+    if (error) {
+      next(error);
+    } else {
+      lambdaDb.get(
+        `SELECT * FROM users WHERE users.id = ${req.params.userId}`,
+        (error, user) => {
+          res.status(200).send("User deleted. ");
+        }
+      );
+    }
+  });
+});
 
 module.exports = usersRouter;
--- BackEnd/database/schema.js
+++ BackEnd/database/schema.js
@@ -3,7 +3,7 @@ const sqlite3 = require("sqlite3");
 const lambdaDb = new sqlite3.Database("./lambdaDb.db");
 
 lambdaDb.serialize(() => {
-  // lambdaDb.run("DROP TABLE IF EXISTS users,");
+  lambdaDb.run("DROP TABLE IF EXISTS users;");
 
   lambdaDb.run(
     `CREATE TABLE users (
@@ -13,31 +13,33 @@ lambdaDb.serialize(() => {
   salt TEXT NO NULL,
   created_at DATETIME NOT NULL,
   updated_at DATETIME NOT NULL
-)`
+);`
   );
-
+  lambdaDb.run("DROP TABLE IF EXISTS admin;");
   lambdaDb.run(
     `CREATE TABLE admin (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
    FOREIGN KEY (user_id) REFERENCES users(id)
-  )`
+  );`
   );
-
+  lambdaDb.run("DROP TABLE IF EXISTS sessions;");
   lambdaDb.run(
     `CREATE TABLE sessions (
     uuid TEXT PRIMARY KEY,
     created_at DATETIME NOT NULL,
     user_id INTEGER
-  )`
+  );`
   );
 
+  lambdaDb.run("DROP TABLE IF EXISTS searches;");
   lambdaDb.run(`CREATE TABLE searches (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       country_id INTEGER,
       metric_id INTEGER,
       user_id INTEGER,
       start_year DATETIME NOT NULL,
-      end_year DATETIME NOT NULL
-    )`);
+      end_year DATETIME NOT NULL,
+      searched_at DATETIME NOT NULL
+    );`);
 });
