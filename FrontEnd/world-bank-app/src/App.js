1e399b00dd996c1874d9f67ec87296549c75dac2
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2021-10-19 15:39:26 +0100

    App now has routes

--- FrontEnd/world-bank-app/src/App.js
+++ FrontEnd/world-bank-app/src/App.js
@@ -1,16 +1,32 @@
 import "./App.css";
-import UserRegistration from "./Components/UserRegistration";
-import UserLogin from "./Components/UserLogin";
+import React from "react";
+import UserLogin from "./Components/UserLogin.js";
+import UserRegistration from "./Components/UserRegistration.js";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>WORLD BANK PROJECT</h1>
-        <UserLogin />
-        <UserRegistration />
-      </header>
-    </div>
+    <Router>
+      {/* <Header /> */}
+      <div className="App">
+        <main className="App-header">
+          <Switch>
+            <Route path="/login">
+              <UserLogin />
+            </Route>
+            <Route path="/sign-up">
+              <UserRegistration />
+            </Route>
+            <Route path="/">
+              <UserLogin />
+              <UserRegistration />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+      {/* <Footer /> */}
+    </Router>
   );
 }
 
