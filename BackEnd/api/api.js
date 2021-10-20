56de2fa293fae9d74488c6005e7d2d992029cb49
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2021-10-20 15:59:05 +0100

    Backend searching works

--- BackEnd/api/api.js
+++ BackEnd/api/api.js
@@ -6,11 +6,11 @@ const apiRouter = express.Router();
 const usersRouter = require("./users.js");
 //const adminRouter = require("./admin.js");
 const sessionsRouter = require("./sessions.js");
-//const searchesRouter = require("./votes.js");
+const searchesRouter = require("./searches.js");
 
 apiRouter.use("/users", usersRouter);
 //apiRouter.use("/admin", adminRouter);
 apiRouter.use("/sessions", sessionsRouter);
-//apiRouter.use("/searches", searchesRouter);
+apiRouter.use("/searches", searchesRouter);
 
 module.exports = apiRouter;
---
+++ BackEnd/database/sqlcols.js
@@ -0,0 +1,27 @@
+// /database/sqlcols.js
+
+/*
+
+-- TABLE: 
+--			column1, column2, etc.
+----------------------------------------------------
+
+-- Country:
+-- 			CountryCode, ShortName, TableName, LongName, Alpha2Code, CurrencyUnit, SpecialNotes,  Region, IncomeGroup, Wb2Code, NationalAccountsBaseYear, NationalAccountsReferenceYear, SnaPriceValuation, LendingCategory, OtherGroups, SystemOfNationalAccounts, AlternativeConversionFactor, PppSurveyYear, BalanceOfPaymentsManualInUse, ExternalDebtReportingStatus, SystemOfTrade, GovernmentAccountingConcept, ImfDataDisseminationStandard, LatestPopulationCensus, LatestHouseholdSurvey, SourceOfMostRecentIncomeAndExpenditureData, VitalRegistrationComplete, LatestAgriculturalCensus, LatestIndustrialData, LatestTradeData, LatestWaterWithdrawalData
+
+-- CountryNotes:
+-- 			Countrycode, Seriescode, Description
+
+-- Footnotes:
+--			Countrycode, Seriescode, Year, Description
+
+-- Indicators:
+--			CountryName, CountryCode, IndicatorName, IndicatorCode, Year, Value
+
+-- Series:
+--			SeriesCode, Topic, IndicatorName, ShortDefinition,  LongDefinition, UnitOfMeasure, Periodicity, BasePeriod, OtherNotes, AggregationMethod, LimitationsAndExceptions, NotesFromOriginalSource, GeneralComments, Source, StatisticalConceptAndMethodology, DevelopmentRelevance, RelatedSourceLinks, OtherWebLinks, RelatedIndicators, LicenseType
+
+-- SeriesNotes: 
+--			Seriescode, Year, Description
+
+*/
