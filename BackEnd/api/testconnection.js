5f898a0c835328388a7709e2b88e9914b0420a26
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2021-10-21 12:45:24 +0100

    Back end searching now working with remote db

---
+++ BackEnd/api/testconnection.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const searchesRouter = express.Router();
+const sqlite3 = require("sqlite3");
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const { Pool, Client } = require("pg");
+
+const connectionString =
+  "postgres://czreijar:TJ2StTuQIl2CoRoinQTwPxk8pBGfdf6t@kandula.db.elephantsql.com/czreijar";
+const pool = new Pool({
+  connectionString,
+});
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.log(err);
+  }
+  pool.end();
+});
+const client = new Client({
+  connectionString,
+});
+client.connect();
+client.query(
+  "SELECT value FROM indicators WHERE countrycode = 'GBR' LIMIT 1",
+  (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const country = res.rows;
+      console.log(country);
+    }
+    client.end();
+  }
+);
