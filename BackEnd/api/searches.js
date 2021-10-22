abeee215da3cda69061ca2ac10242f5e044d4170
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2021-10-22 11:33:58 +0100

    msdngksn

--- BackEnd/api/searches.js
+++ BackEnd/api/searches.js
@@ -94,26 +94,27 @@ searchesRouter.get("/years", async (req, res, next) => {
 });
 
 // Get search history for particular user
-searchesRouter.get('/history', async(req, res, next) => {
-    const uuid = req.cookies.sessionId;
-    const sql = "SELECT user_id FROM sessions WHERE uuid = $uuid";
-      const val = { $uuid: uuid };
-      lambdaDb.get(sql, val, (err, row) => {
+searchesRouter.get("/history", async (req, res, next) => {
+  const uuid = req.cookies.sessionId;
+  const sql = "SELECT user_id FROM sessions WHERE uuid = $uuid";
+  const val = { $uuid: uuid };
+  lambdaDb.get(sql, val, (err, row) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const { user_id } = row;
+      const sql2 = `SELECT * FROM searches WHERE user_id = $user_id`;
+      const val2 = { $user_id: user_id };
+      lambdaDb.all(sql2, val2, (err, rows) => {
         if (err) {
           console.log(err);
         } else {
-            const { user_id } = row;
-            const sql2 = `SELECT * FROM searches WHERE user_id = $user_id`
-            const val2 = {$user_id: user_id}
-            lambdaDb.all(sql2, val2, (err, rows) => {
-              if (err){
-                console.log(err)
-              } else {
-                res.send({rows: rows})
-              }
-            })
+          res.send({ rows: rows });
         }
-})
+      });
+    }
+  });
+});
 
 // Post user search
 searchesRouter.post("/", async (req, res, next) => {
