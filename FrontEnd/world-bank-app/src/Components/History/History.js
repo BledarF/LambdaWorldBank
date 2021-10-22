123e7fff958e3c4871b647aa134b0b1650f5c89d
BledarF <88328574+BledarF@users.noreply.github.com>
2021-10-22 11:47:23 +0100

    Fixed up handling history in frontend

--- FrontEnd/world-bank-app/src/Components/History/History.js
+++ FrontEnd/world-bank-app/src/Components/History/History.js
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function History() {
-  const [history, setHistory] = useState();
+  const [history, setHistory] = useState("");
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  async function fetchAPI() {
+    await getHistory();
+  }
 
   async function getHistory() {
     const url = "http://localhost:7000/api/searches/history";
-    const addResponse = await fetch(url);
+    const addResponse = await fetch(url, {
+      credentials: "include",
+    });
 
     const jsonResponse = await addResponse.json();
+    console.log(jsonResponse);
 
-    setHistory(jsonResponse.history); //history should be returned as an object returning search
+    setHistory(jsonResponse); //history should be returned as an object returning search
     //title search time/date? etc.
   }
-  const { countrySelected, indicatorSelected, startYear, endYear } = history;
+  // const { countrySelected, indicatorSelected, startYear, endYear } = history;
 
-  return (
-    <div>
-      <h3>{countrySelected}</h3>
-      <p>{indicatorSelected}</p>
-      <p>{startYear}</p>
-      <p>{endYear}</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default History;
